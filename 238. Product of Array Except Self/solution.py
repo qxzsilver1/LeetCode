e23f115e839e3ada8ce9f746b989c5752f92e795
qxzsilver1 <hk442@cornell.edu>
2024-01-07 18:49:58 -0500

    Update solution.py
    Update from O(cn) memory complexity to O(1) while keeping O(n) time complexity

--- 238. Product of Array Except Self/solution.py
+++ 238. Product of Array Except Self/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix_prod = [1] * len(nums)
-        suffix_prod = [1] * len(nums)
         prod = [1] * len(nums)
-        
-        for i in range(1, len(nums)):
-            prefix_prod[i] = nums[i-1] * prefix_prod[i-1]
-        
-        for i in range(len(nums) - 2, -1, -1):
-            suffix_prod[i] = suffix_prod[i+1] * nums[i+1]
-        
+
+        prefix = 1
+        suffix = 1
+
         for i in range(len(nums)):
-            prod[i] = prefix_prod[i] * suffix_prod[i]
+            prod[i] = prefix
+            prefix *= nums[i]
+        
+        for i in range(len(nums)-1, -1, -1):
+            prod[i] *= suffix
+            suffix *= nums[i]
         
         return prod
