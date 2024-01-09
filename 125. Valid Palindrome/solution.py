3fcdce7693b2d8d0691cdee3da4c45fa30fb1403
qxzsilver1 <hk442@cornell.edu>
2024-01-09 04:04:06 -0500

    Update solution.py
    Update to optimal soln

--- 125. Valid Palindrome/solution.py
+++ 125. Valid Palindrome/solution.py
@@ -1,19 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
+        new_str = ''
 
-        while l < r:
-            while l < r and not self.is_alphanumeric(s[l]):
-                l += 1
-            while l < r and not self.is_alphanumeric(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            
-            l, r = l+1, r-1
+        for c in s:
+            if c.isalnum():
+                new_str += c.lower()
         
-        return True
-
-    
-    def is_alphanumeric(self, c: str) -> bool:
-        return (ord('A') <= ord(c) <= ord('Z') or ord('0') <= ord(c) <= ord('9') or ord('a') <= ord(c) <= ord('z'))
+        return new_str == new_str[::-1]
