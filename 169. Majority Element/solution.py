c7a4476ec97745c0918b09c5d26424ca9d9e6115
qxzsilver1 <hk442@cornell.edu>
2024-01-09 16:29:46 -0500

    Update solution.py
    Boyer-Moore algorithm to use O(1) space instead of O(n) for hashmap

--- 169. Majority Element/solution.py
+++ 169. Majority Element/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = dict()
-        output, max_count = 0, 0
+        output, count = 0, 0
 
         for n in nums:
-            counts[n] = 1 + counts.get(n, 0)
-
-            output = n if counts[n] > max_count else output
-            max_count = max(counts[n], max_count)
+            if count == 0:
+                output = n
+            
+            count += 1 if n == output else -1
         
         return output
