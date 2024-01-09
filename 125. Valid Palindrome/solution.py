6aeeb3e2dff7a71140ef4a6b791374fccd030452
qxzsilver1 <hk442@cornell.edu>
2024-01-09 04:02:53 -0500

    Update solution.py
    Update with two pointers approach and non-library function

--- 125. Valid Palindrome/solution.py
+++ 125. Valid Palindrome/solution.py
@@ -1,9 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new_str = ''
+        l, r = 0, len(s) - 1
 
-        for c in s:
-            if c.isalnum():
-                new_str += c.lower()
+        while l < r:
+            while l < r and not self.is_alphanumeric(s[l]):
+                l += 1
+            while l < r and not self.is_alphanumeric(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l, r = l+1, r-1
         
-        return new_str == new_str[::-1]
+        return True
+
+    
+    def is_alphanumeric(self, c: str) -> bool:
+        return (ord('A') <= ord(c) <= ord('Z') or ord('0') <= ord(c) <= ord('9') or ord('a') <= ord(c) <= ord('z'))
