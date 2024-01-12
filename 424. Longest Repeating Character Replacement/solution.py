27e68503ae2b710be25c8eb3eb2248f807ecead2
qxzsilver1 <hk442@cornell.edu>
2024-01-11 20:45:43 -0500

    Update solution.py
    max_freq optimization to not go through count.values() which is O(n) extra time

--- 424. Longest Repeating Character Replacement/solution.py
+++ 424. Longest Repeating Character Replacement/solution.py
@@ -2,13 +2,15 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
         output = 0
+        max_freq = 0
 
         l = 0
 
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-
-            while (r - l + 1) - max(count.values()) > k:
+            max_freq = max(max_freq, count[s[r]])
+            
+            while (r - l + 1) - max_freq > k:
                 count[s[l]] -= 1
                 l += 1
 
