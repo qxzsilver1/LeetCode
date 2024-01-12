2de328279334a9dc197dede6e8b26527e58b88c4
qxzsilver1 <hk442@cornell.edu>
2024-01-11 22:42:07 -0500

    Create solution_bit_logic.py
    bit logic using number and 1 subtracted from itself - uses trick to get rid of 1-bits each time using AND (&) bit operation

---
+++ 191. Number of 1 Bits/solution_bit_logic.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        bit_count = 0
+
+        while n:
+            n = n & (n-1)
+            bit_count += 1
+        
+        return bit_count
