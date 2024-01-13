2dbae3aadac2bc12384eb2ef61c73932c4a607e0
qxzsilver1 <hk442@cornell.edu>
2024-01-13 18:18:42 -0500

    Update solution.py
    Fix error in index out of bounds exception for right pointer being out of bounds

--- 540. Single Element in a Sorted Array/solution.py
+++ 540. Single Element in a Sorted Array/solution.py
@@ -5,7 +5,7 @@ class Solution:
         while l <= r:
             m = l + (r - l) // 2
 
-            if (m - 1 < 0 or nums[m-1] != nums[m]) and (m > len(nums) - 1 or nums[m+1] != nums[m]):
+            if (m - 1 < 0 or nums[m-1] != nums[m]) and (m >= len(nums) - 1 or nums[m+1] != nums[m]):
                 return nums[m]
             
             left_size = m - 1 if nums[m-1] == nums[m] else m
