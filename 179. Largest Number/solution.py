31a137dc4daa4513d708029407a192253c2f0783
qxzsilver1 <hk442@cornell.edu>
2024-01-14 02:14:01 -0500

    Create solution.py

---
+++ 179. Largest Number/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i, n in enumerate(nums):
+            nums[i] = str(n)
+        
+        def compare(ns1, ns2):
+            if ns1 + ns2 > ns2 + ns1:
+                return -1
+            else:
+                return 1
+
+        nums = sorted(nums, key= cmp_to_key(compare))
+
+        return str(int(''.join(nums)))
