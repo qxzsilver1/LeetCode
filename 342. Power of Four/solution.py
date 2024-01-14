e6e6c4441d8461514aa0568a065922a9a7f5523f
qxzsilver1 <hk442@cornell.edu>
2024-01-13 19:57:27 -0500

    Update solution.py
    (almost) O(1) time solution

--- 342. Power of Four/solution.py
+++ 342. Power of Four/solution.py
@@ -1,9 +1,3 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n == 1:
-            return True
-        
-        if n <= 0 or n % 4:
-            return False
-
-        return self.isPowerOfFour(n // 4)
+        return n > 0 and log(n, 4) % 1 == 0
