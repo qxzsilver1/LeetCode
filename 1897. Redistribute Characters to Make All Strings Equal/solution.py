1a5dbe6fccd1c082188022e5a22b708a959d1076
qxzsilver1 <hk442@cornell.edu>
2024-01-14 21:19:08 -0500

    Update solution.py
    Change to defaultdict() instead to be less verbose, and check val % n instead of needing to compare to 0.
    
    This algo is still O(m*n), where n is length of lists, and m = max(length of each word in words). Space is O(26) = O(1)

--- 1897. Redistribute Characters to Make All Strings Equal/solution.py
+++ 1897. Redistribute Characters to Make All Strings Equal/solution.py
@@ -1,14 +1,14 @@
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
-        count_dict = {}
+        count_dict = defaultdict(int)
         n = len(words)
 
         for word in words:
             for c in word:
-                count_dict[c] = 1 + count_dict.get(c, 0)
+                count_dict[c] += 1
 
         for val in count_dict.values():
-            if val % n != 0:
+            if val % n:
                 return False
 
         return True 
