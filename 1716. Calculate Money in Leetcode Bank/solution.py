e4114ff602b211c0bf45110f5874ebaf6af06755
qxzsilver1 <hk442@cornell.edu>
2024-01-15 02:01:31 -0500

    Update solution.py
    more efficient solution for calculating weekly sums using summation formula

--- 1716. Calculate Money in Leetcode Bank/solution.py
+++ 1716. Calculate Money in Leetcode Bank/solution.py
@@ -1,15 +1,13 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        day = 0
-        deposit = 1
-        output = 0
+        num_weeks = n // 7
+        low = 28
+        high = low + 7 * (num_weeks - 1)
+        output = (low + high) * num_weeks // 2
 
-        while day < n:
-            output += deposit
-            deposit += 1
-            day += 1
+        monday = num_weeks + 1
 
-            if day % 7 == 0:
-                deposit = day // 7 + 1
-        
+        for i in range(n % 7):
+            output += i + monday
+
         return output
