76ff6b108285dccd8fe45151264324579b24c11e
qxzsilver1 <hk442@cornell.edu>
2024-01-17 00:37:16 -0500

    Update solution.py
    Corrected: previous failed at two-way mappings. Still O(n) time complexity 
    
    create an O(1) space of 128 character mappings of indices from the ord() values from s -> t and t -> s.
    
    If both are set at -1 (default value), then set their index pointers to each other, respectively.
    
    If either do not match, then return False.
    
    After loop, return True

--- 205. Isomorphic Strings/solution.py
+++ 205. Isomorphic Strings/solution.py
@@ -1,12 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        s_to_t = {}
-        i, n = 0, len(s)
+        s_to_t = [-1] * 128
+        t_to_s = [-1] * 128
+        n = len(s)
 
         for i in range(n):
-            if s[i] not in s_to_t:
-                s_to_t[s[i]] = t[i]
-            elif s_to_t[s[i]] != t[i]:
+            s_index = ord(s[i])
+            t_index = ord(t[i])
+
+            if s_to_t[s_index] == -1 and t_to_s[t_index] == -1:
+                s_to_t[s_index] = t_index
+                t_to_s[t_index] = s_index
+            
+            if s_to_t[s_index] != t_index or t_to_s[t_index] != s_index:
                 return False
         
         return True
