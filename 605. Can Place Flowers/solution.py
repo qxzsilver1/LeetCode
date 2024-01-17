4fdf0a9e67dd6e808d83d085ee9d44026d947bf3
qxzsilver1 <hk442@cornell.edu>
2024-01-17 11:30:25 -0500

    Update solution.py
    O(n) time and O(1) space solution
    
    keep contiguous empty counter and initialize to 0 if there is a flower at beginning of flowerbed array (otherwise 1)
    
    for each flower, if the flower is not empty, decrement empty counter (rounded to nearest 0, python style) and then adjust n by this amount and set empty counter to 0. In the case where there is 1 empty, this will round down to 0
    
    otherwise, keep incrementing empty counter
    
    finally, subtract n by how many contiguous empty counters there are, and if n is exhausted, return true

--- 605. Can Place Flowers/solution.py
+++ 605. Can Place Flowers/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        f_len = len(flowerbed)
-        f = [0] + flowerbed + [0]
+        contiguous_empty = 0 if flowerbed[0] else 1
 
-        for i in range(1, f_len+1):
-            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
-                f[i] = 1
-                n -= 1
+        for f in flowerbed:
+            if f:
+                n -= int((contiguous_empty - 1) / 2) # round int division towards 0 - python
+                contiguous_empty = 0
             else:
-                continue
+                contiguous_empty += 1
+        
+        n -= (contiguous_empty) // 2 
         
         return n <= 0
