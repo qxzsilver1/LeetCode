5b3a16b520fca8d25919bc9ab75c5dd78c084369
qxzsilver1 <hk442@cornell.edu>
2024-01-19 15:49:50 -0500

    Update solution.py
    O(n) time and O(1) space solution
    
    create two arrays counting occurrences of letters 'a', 'b', 'l', 'n', 'o'.  Set minimum res as length of the input string. Go through the input string and count all such occurrences using the helper function. Then, iterate through the length == 5 string of text counts of the letters and balloon counter to find the minimum ratio

--- 1189. Maximum Number of Balloons/solution.py
+++ 1189. Maximum Number of Balloons/solution.py
@@ -1,25 +1,27 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
+        balloon = 'ablno'
         balloon_ablno_count = [1, 1, 2, 1, 2]
-        ctr = 0
+        text_ablno_count = [0, 0, 0, 0, 0]
+
+        res = len(text)
 
         def balloon_counter(char):
             if char == 'a':
-                balloon_ablno_count[0] -= 1
+                text_ablno_count[0] += 1
             elif char == 'b':
-                balloon_ablno_count[1] -= 1
+                text_ablno_count[1] += 1
             elif char == 'l':
-                balloon_ablno_count[2] -= 1
+                text_ablno_count[2] += 1
             elif char == 'n':
-                balloon_ablno_count[3] -= 1
+                text_ablno_count[3] += 1
             elif char == 'o':
-                balloon_ablno_count[4] -= 1
+                text_ablno_count[4] += 1
 
         for t in text:
             balloon_counter(t)
+        
+        for i in range(len(balloon)):
+            res = min(res, text_ablno_count[i] // balloon_ablno_count[i])
 
-            if all(cnt == 0 for cnt in balloon_ablno_count):
-                ctr += 1
-                balloon_ablno_count = [1, 1, 2, 1, 2]
-
-        return ctr
+        return res
