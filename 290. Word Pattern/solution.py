6038478a349378d61fcbb2c2c6a6bcb7aeca90c0
qxzsilver1 <hk442@cornell.edu>
2024-01-19 19:09:32 -0500

    Update solution.py
    O(m) solution where m is number of words in the list. Create O(1) = O(26) size array for the pattern_to_str mapping

--- 290. Word Pattern/solution.py
+++ 290. Word Pattern/solution.py
@@ -5,15 +5,17 @@ class Solution:
         if len(words) != len(pattern):
             return False
         
-        pattern_to_str = {}
+        pattern_to_str = [''] * 26
         str_to_pattern = {}
 
         for i in range(len(pattern)):
-            if pattern[i] not in pattern_to_str and words[i] not in str_to_pattern:
-                pattern_to_str[pattern[i]] = words[i]
-                str_to_pattern[words[i]] = pattern[i]
+            ord_val = ord(pattern[i]) - ord('a')
             
-            if pattern_to_str[pattern[i]] != words[i] or str_to_pattern[words[i]] != pattern[i]:
+            if not pattern_to_str[ord_val] and words[i] not in str_to_pattern:
+                pattern_to_str[ord_val] = words[i]
+                str_to_pattern[words[i]] = chr(ord_val + ord('a'))
+            
+            if pattern_to_str[ord_val] != words[i] or str_to_pattern[words[i]] != pattern[i]:
                 return False
         
         return True
