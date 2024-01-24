72c703d4b9ed20a9bc65ec8618e807bc79cdf795
qxzsilver1 <hk442@cornell.edu>
2024-01-24 13:59:52 -0500

    Update solution.py
    O(n) where n is the min length of (s1, s2, s3). Find the longest subsequence among them and return the sum of lengths of the strings subtracted by 3 times the longest (this is the number of operations that would need to be done to keep the 3 longest substrings in each string)

--- 2937. Make Three Strings Equal/solution.py
+++ 2937. Make Three Strings Equal/solution.py
@@ -1,44 +1,17 @@
 class Solution:
     def findMinimumOperations(self, s1: str, s2: str, s3: str) -> int:
-        string_set = set()
-        string_set.add(s1)
-        string_set.add(s2)
-        string_set.add(s3)
-        
-        len1, len2, len3 = len(s1), len(s2), len(s3)
-        len_matrix = [len1, len2, len3]
+        longest = -1
 
-        idx, min_len = -1, float('inf')
+        for i in range(min(len(s1), len(s2), len(s3))):
+            if not s1[i] == s2[i] == s3[i]:
+                break
+            else:
+                longest = i
+        
+        if longest < 0:
+            return -1
+        
 
-        for i, l in enumerate(len_matrix):
-            if min_len > l:
-                min_len = l
-                idx = i
-        
-        shortest_str = ''
+        longest += 1
 
-        if idx == 0:
-            shortest_str = s1
-        elif idx == 1:
-            shortest_str = s2
-        elif idx == 2:
-            shortest_str = s3
-
-        non_shortest_str_set = string_set.difference(shortest_str)
-        non_shortest_str_list = list(non_shortest_str_set)
-        non_shortest_str_list_len = len(non_shortest_str_list)
-        
-        for j in range(min_len):
-            for non_short_str in non_shortest_str_list:
-                if shortest_str[j] != non_short_str[j]:
-                    return -1
-                
-                continue
-        
-        res = 0
-
-        for k in range(non_shortest_str_list_len):
-            res += len(non_shortest_str_list[k]) - min_len
-        
-        return res
-        
+        return len(s1) + len(s2) + len(s3) - 3 * longest
