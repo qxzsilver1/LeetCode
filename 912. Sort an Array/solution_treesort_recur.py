dd536ff4cffcbd37bd1fdc58e2ffba4eb6792c30
qxzsilver1 <hk442@cornell.edu>
2024-02-04 18:43:30 -0500

    Create solution_treesort_recur.py
    Recursive O(n log n) solution with O(n) extra space for tree and output array.
    
    insert() inserts to left or right child recursively for the tree. This is iterated across the given nums list after setting root to the first element.
    
    Next, an inorder traversal through this binary search tree that was built is output into the output array.

---
+++ 912. Sort an Array/solution_treesort_recur.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        output = []
+
+        def insert(node, val):
+            if not node:
+                return TreeNode(val)
+            
+            if val <= node.val:
+                node.left = insert(node.left, val)
+            else:
+                node.right = insert(node.right, val)
+            
+            return node
+        
+        root = TreeNode(nums[0])
+
+        for i in range(1, len(nums)):
+            insert(root, nums[i])
+        
+        def inorder(root):
+            if not root:
+                return
+            
+            inorder(root.left)
+            output.append(root.val)
+            inorder(root.right)
+        
+        inorder(root)
+
+        return output
