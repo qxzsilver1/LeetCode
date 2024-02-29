d1aaa38277cf1b16f416e586ce11ef94302e719a
qxzsilver1 <hk442@cornell.edu>
2024-02-28 21:09:33 -0500

    Create solution_sqrt.py
    Implementation using int(sqrt(n)) instead of isqrt(n). Same time complexity and space complexity

---
+++ 2778. Sum of Squares of Special Elements/solution_sqrt.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        n = len(nums)
+        int_sqrt_n = int(sqrt(n))
+        print(int_sqrt_n)
+
+        res = 0
+
+        for i in range(int_sqrt_n):
+            if n % (i + 1):
+                continue
+            
+            res += nums[i] ** 2 + nums[n // (i+1) - 1] ** 2 if i + 1 != n // (i + 1) else nums[i] ** 2
+        
+        return res
