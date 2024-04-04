e3967593b879625023db63013a041164c0b381e3
qxzsilver1 <hk442@cornell.edu>
2024-04-04 04:29:44 -0400

    Create solution_bs.py
    O(log(n)) time solution using binary search. Set left and right pointers to 1 and num, then iterate midpoint at each successive iteration. This is faster than O(sqrt(n)).

---
+++ 367. Valid Perfect Square/solution_bs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l, r = 1, num
+
+        while l <= r:
+            m = l + (r - l) // 2
+            
+            if m * m > num:
+                r = m - 1
+            elif m * m < num:
+                l = m + 1
+            else:
+                return True
+        
+        return False
+            
