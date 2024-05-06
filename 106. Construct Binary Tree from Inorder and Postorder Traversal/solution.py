a06c9b250452d6cceea91956ae2bc5a14ce431f0
qxzsilver1 <hk442@cornell.edu>
2024-05-06 16:09:42 -0400

    Create solution.py
    O(n) time and O(n) space solution.
    
    First, initialize a dictionary with the values in the inorder array (since they are unique) as keys with the corresponding indices as the values. This will allow for an O(n) time solution (as opposed to an O(n^2) solution when searching for the index in the inorder array as well as slicing the array.
    
    Define a helper function, passing in the leftmost and rightmost indices of the inorder array to focus on. Then, if the pointers cross, then return None. Otherwise, get the root from the postorder array (since it is the rightmost value in the postorder array), then find the index based on the root value from the hashmap. Similarly, recursively call on the left and right indices portion with relation to the index from the inorder values.
    
    Finally, call the helper function with the endpoints as the indices.

---
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        inorder_idx = { v: i for i, v in enumerate(inorder)}
+
+        def helper(l, r):
+            if l > r:
+                return None
+
+            root = TreeNode(postorder.pop())
+
+            idx = inorder_idx[root.val]
+            root.right = helper(idx + 1, r)
+            root.left = helper(l, idx - 1)
+
+            return root
+        
+        return helper(0, len(inorder) - 1)
