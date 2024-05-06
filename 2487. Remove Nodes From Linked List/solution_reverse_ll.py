b8e74849ab578e634e5d7a838d6fbf465109a023
qxzsilver1 <hk442@cornell.edu>
2024-05-06 14:23:19 -0400

    Create solution_reverse_ll.py
    O(n) time and O(1) space solution using reversing the linked list.
    
    First, reverse the linked list using a function to start from the end of the linked list.
    
    The current maximum value is set to the value at the end of the linked list, then while current's next pointer node is not empty, if the current max is greater, skip that node. Otherwise, update the current max and increment the pointer.
    
    Finally, reverse the list (since it was reversed) and return.

---
+++ 2487. Remove Nodes From Linked List/solution_reverse_ll.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        def reverse(head):
+            prev, curr = None, head
+
+            while curr:
+                tmp = curr.next
+                curr.next = prev
+                prev, curr = curr, tmp
+            
+            return prev
+        
+        head = reverse(head)
+        curr = head
+        curr_max = curr.val
+
+        while curr.next:
+            if curr.next.val < curr_max:
+                curr.next = curr.next.next
+            else:
+                curr_max = curr.next.val
+                curr = curr.next
+
+        return reverse(head)
