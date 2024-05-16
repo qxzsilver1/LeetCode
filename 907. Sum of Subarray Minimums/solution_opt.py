080f7d91e38a40fed382b9a236dc59e47e24fbbd
qxzsilver1 <hk442@cornell.edu>
2024-05-16 16:08:43 -0400

    Create solution_opt.py
    O(n) time and space solution using a monotonic stack, but with smaller constant (no need to run through stack again).
    
    Use a sandwiched array by book-ending -infinity values to the array. This makes it so that the stack gets emptied and there is no need to run through the stack for any remaining values again.

---
+++ 907. Sum of Subarray Minimums/solution_opt.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        mod = 10 ** 9 + 7
+        res = 0
+
+        arr = [float('-inf')] + arr + [float('-inf')]
+
+        stack = []
+
+        for i, n in enumerate(arr):
+
+            while stack and n < stack[-1][1]:
+                j, m = stack.pop()
+
+                left = j - stack[-1][0] if stack else j + 1
+                right = i - j
+
+                res = (res + m * left * right) % mod
+
+
+            stack.append((i, n))
+
+        return res
