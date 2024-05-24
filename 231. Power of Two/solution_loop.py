ca65275acb69b689b4f0b1f5f0df8ebfe2f681a8
qxzsilver1 <hk442@cornell.edu>
2024-05-24 17:40:38 -0400

    Create solution_loop.py
    O(1) time and space solution (there is a maximum of 32 iterations in the loop due to 2^32 being the max value (technically, 2^31 - 1).
    
    Set x to 1.
    
    While x is less than n, if x and n are equal, return true. Otherwise, multiply x by 2.
    
    Finally, return whether x equals n.

---
+++ 231. Power of Two/solution_loop.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        x = 1
+
+        while x < n:
+            if x == n:
+                return True
+            
+            x *= 2
+        
+        return x == n
