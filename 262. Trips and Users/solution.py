ecc433d5bd757e8f61b973b2173934a9e8d7b139
qxzsilver1 <hk442@cornell.edu>
2024-06-10 14:04:34 -0400

    Create solution.py
    Use a CTE (common table expression) not_banned that contains all users whose users_id are not banned.
    
    Then, select the request_at and assign as Day, and the rounded sum if the status is either cancelled by driver or client, assigned a 1 for theses cases and 0 otherwise, then divided by the total count and rounded to 2 decimal places. These cases are selected when both client ID and driver ID are in the not_banned CTE, and the request is between the dates 2013-10-01 to 2013-10-03. The cases are grouped by the request dates.

---
+++ 262. Trips and Users/solution.py
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+WITH not_banned AS (
+    SELECT users_id from Users
+    WHERE banned='No'
+)
+
+SELECT request_at AS Day, 
+ROUND(SUM(CASE WHEN status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1.00 ELSE 0 END) / COUNT(*), 2) AS 'Cancellation Rate'
+FROM Trips
+WHERE client_id IN (SELECT users_id FROM not_banned) 
+AND driver_id IN (SELECT users_id FROM not_banned) 
+AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at;
