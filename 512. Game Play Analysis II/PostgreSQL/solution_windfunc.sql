06e156fb5239f7cdc86328b4ea81cc3654b8d7d7
qxzsilver1 <hk442@cornell.edu>
2024-06-19 23:20:49 -0400

    Create solution_windfunc.sql

---
+++ 512. Game Play Analysis II/PostgreSQL/solution_windfunc.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT player_id, device_id
+FROM (
+    SELECT player_id, device_id, event_date,
+    ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS event_row FROM Activity
+) t
+WHERE t.event_row=1;
