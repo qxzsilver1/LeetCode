b2564c09feafc932b2eebcc7dd9bee4065513339
qxzsilver1 <hk442@cornell.edu>
2024-09-03 08:59:38 -0400

    Create solution_iter_opt.py
    O(n) time solution with O(1) space using two variables to store the two states in iteration.
    
    First, if n is 0 or 1, return the number.
    
    Set values a and b to 0 and 1 (the default (0-th) value and 1st value of the Fibonacci series), respectively.
    
    For each number from 2 to n, set c to the sum of a and b, and reset a to b and b to c.
    
    Finally, return c.

---
+++ 509. Fibonacci Number/solution_iter_opt.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        if n == 0 or n == 1:
+            return n;
+        
+        a, b = 0, 1
+
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+        
+        return c
