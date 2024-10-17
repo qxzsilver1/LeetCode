52efa4199af9d2182b765bcc7f020fde10e2ba27
qxzsilver1 <hk442@cornell.edu>
2024-10-16 20:25:56 -0400

    Create solution_str_slice.py
    O(n + m) and O(1) space using string slicing.
    
    Define the left and right rotate functions.
    
    Get the mod size and the shift number, then for each direction/amount pair in the shift array, if direction is 0 (left-rotate), increment to the shift number, otherwise (if the direction is 1, i.e., right-rotate), decrement from the shift number.
    
    Mod the shift number by the length of the string.
    
    If the shift number is positive, left rotate, and if it is negative, right rotate. Nothing is done if it is 0.
    
    Return the string.

---
+++ 1427. Perform String Shifts/solution_str_slice.py
@@ -0,0 +1,27 @@
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        def leftRotate(s, amt):
+            return s[amt:] + s[0:amt]
+        
+        def rightRotate(s, amt):
+            return leftRotate(s, len(s) - amt)
+        
+        mod_size = len(s)
+
+        shift_num = 0
+
+        for direction, amount in shift:
+            if direction == 0:
+                shift_num += amount
+            else:
+                shift_num -= amount
+        
+        shift_num %= mod_size
+
+        if shift_num > 0:
+            s = leftRotate(s, shift_num)
+        elif shift_num < 0:
+            s = rightRotate(s, - shift_num)
+        
+        return s
+        
