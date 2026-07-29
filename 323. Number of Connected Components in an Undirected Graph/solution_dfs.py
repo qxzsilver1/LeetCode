a6151e87cf0926d6896697b754fb20fcc50ac159
qxzsilver1 <hk442@cornell.edu>
2026-07-29 17:05:40 -0400

    Create solution_dfs.py
    O(E + V) time and space complexity DFS solution with adjacency list and visited list, along with a result counter for each DFS run.

---
+++ 323. Number of Connected Components in an Undirected Graph/solution_dfs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj_list = [[] for _ in range(n)]
+
+        visited = [False] * n
+
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+        
+        def dfs(node):
+            for nei in adj_list[node]:
+                if not visited[nei]:
+                    visited[nei] = True
+                    dfs(nei)
+        
+        res = 0
+
+        for i in range(n):
+            if not visited[i]:
+                visited[i] = True
+                dfs(i)
+                res += 1
+        
+        return res
