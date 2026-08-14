e64bba84aa3664f9d6884641bba861a46f7dad08
qxzsilver1 <hk442@cornell.edu>
2026-08-14 10:38:12 -0400

    Create solution_hash_table.py
    O(nkw - (kw)^2) time and O(k + w) space solution using a hash table, where n is the length of the string s, k is the number of words in the words list, and w is the length of a given word.
    
    Initialize n, k, word length, the possible substring length, and a word count dictionary.
    
    Define the check function starting from index i. Get the remaining word count by copying the dictionary. Set the number of words used to 0.
    
    For each index starting from i to the length of the substring incrementing by the length of any given word, set the substring.
    
    If the substring is remaining in the count (is greater than 0), then decrement this from the dictionary and increment the word used. Otherwise, break.
    
    Return whether all words were used (words used is equal to k).
    
    Initialize the result array.
    
    For each i in the range of n - the length of the substring length + 1, if checking at the index is true, append i to the result.
    
    Finally, return the result.

---
+++ 30. Substring with Concatenation of All Words/solution_hash_table.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        n = len(s)
+        k = len(words)
+
+        word_len = len(words[0])
+        substring_len = word_len * k
+
+        word_count = Counter(words)
+
+        def check(i):
+            remaining = word_count.copy()
+            words_used = 0
+
+            for j in range(i, i + substring_len, word_len):
+                sub = s[j:j + word_len]
+
+                if remaining[sub] > 0:
+                    remaining[sub] -= 1
+                    words_used += 1
+                else:
+                    break
+            
+            return words_used == k
+        
+        res = []
+
+        for i in range(n - substring_len + 1):
+            if check(i):
+                res.append(i)
+        
+        return res
