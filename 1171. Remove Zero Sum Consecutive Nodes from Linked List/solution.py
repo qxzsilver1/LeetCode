1c861da5d03bcc1784e58ffbe8b3bc8ac42ca5bb
qxzsilver1 <hk442@cornell.edu>
2026-08-20 10:39:45 -0400

    Create solution.py
    O(n) time and space solution using a prefix sum hash table.
    
    First, create a dummy starting node front with value 0 that has as its next node the head node.
    
    Set the current node to this node, and set the prefix sum to 0. Initialize the prefix sum-to-node mapping hash table with 0 to the front node.
    
    While the current node is not empty (None), increment the prefix sum by the current node's value, and add this prefix sum key with the current node as its value into the hash table. Advance the current node to the next node.
    
    Reset the prefix sum to 0 and set the current node to the front node again.
    
    Now, it is time to delete consecutive sum sequences equaling 0 by iterating through the linked list again.
    
    While the current node is not none, increment the prefix sum by the current value, and set the current node's next node to the value found for that prefix sum's next node. Then, advance the current node to the next node.
    
    Finally, return the next node of the dummy front node (the new head).

---
+++ 1171. Remove Zero Sum Consecutive Nodes from Linked List/solution.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        front = ListNode(0, head)
+
+        curr = front
+        prefix_sum = 0
+        prefix_sum_to_node = {0 : front}
+
+        while curr:
+            prefix_sum += curr.val
+            prefix_sum_to_node[prefix_sum] = curr
+            curr = curr.next
+        
+        prefix_sum = 0
+        curr = front
+
+        while curr:
+            prefix_sum += curr.val
+            curr.next = prefix_sum_to_node[prefix_sum].next
+            curr = curr.next
+        
+        return front.next
