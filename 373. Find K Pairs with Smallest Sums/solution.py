7a17c48fa499eaf0e8d1f82c5c4a80218de907f4
qxzsilver1 <hk442@cornell.edu>
2026-08-20 15:47:07 -0400

    Create solution.py
    O(min(k log k, mn log (mn))) time solution with O(min(k, mn)) space for the min heap, where k is the k-size heap, or m and n are the sizes of the number 1 and 2 arrays. The approach is similar to Dijkstra's algorithm in finding the shortest path using a priority queue by having a visited set.
    
    First, initialize the m and n values.
    
    Initialize an empty result array and an empty visited set.
    
    Create a min heap with the sum of the first 2 numbers of the arrays (the smallest values), with the "coordinates" of (0, 0) and add this to the visited set.
    
    While k is not 0 and the min heap is not empty, pop from the heap and get the number at the coordinate value (indices of the nums1 and nums2 arrays, respectively). 
    
    Append the nums1 and nums2 pair to the result.
    
    If the i+1 (next) index of i is within bounds and the coordinate is not visited, push the sum of the neighbor and the coordinate, and add this to the visited set.
    
    Similarly, if the j+1 (next) index of j is within the bounds of the coordinate and not yet visited, push the new sum to the heap (along with the indices) and add the coordinates to the visited set.
    
    Decrement the k value afterwards.
    
    Finally, return the result.

---
+++ 373. Find K Pairs with Smallest Sums/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        m, n = len(nums1), len(nums2)
+
+        res = []
+
+        visited = set()
+
+        min_heap = [(nums1[0] + nums2[0], (0,0))]
+        visited.add((0, 0))
+
+        while k > 0 and min_heap:
+            num, (i, j) = heapq.heappop(min_heap)
+            res.append([nums1[i], nums2[j]])
+
+            if i + 1 < m and (i+1, j) not in visited:
+                heapq.heappush(min_heap, (nums1[i+1] + nums2[j], (i+1, j)))
+                visited.add((i+1, j))
+            
+            if j + 1 < n and (i, j+1) not in visited:
+                heapq.heappush(min_heap, (nums1[i] + nums2[j+1], (i, j+1)))
+                visited.add((i, j+1))
+            
+            k -= 1
+        
+        return res
