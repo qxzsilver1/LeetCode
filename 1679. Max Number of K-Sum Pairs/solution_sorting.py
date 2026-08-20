c0778567f5940588e2d501fc86f6a6a0fe9fd1bb
qxzsilver1 <hk442@cornell.edu>
2026-08-20 11:06:15 -0400

    Create solution_sorting.py
    O(n log n) time and O(1) (or O(n) space) solution depending on which sorting algorithm is used and using two pointers from either end.
    
    The idea is to sort the values and take advantage of it to get closer to the sum target, and find pairs by the sorted indices.
    
    First, sort the number array.
    
    Initialize the count to 0, and the left and right pointers to the ends of the sorted array.
    
    While the pointers don't cross, if the sum of the numbers at the left and right are less than k, the left pointer value is too small, so increment the left pointer. If the sum of the values are greater than k, the right pointer value is too large so decrement the right pointer. Otherwise, the sum is exactly equal to k, so increment the left pointer and decrement the right pointer both, and increment the count.
    
    Finally, return the count.

---
+++ 1679. Max Number of K-Sum Pairs/solution_sorting.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        cnt = 0
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            if nums[l] + nums[r] < k:
+                l += 1
+            elif nums[l] + nums[r] > k:
+                r -= 1
+            else:
+                l += 1
+                r -= 1
+                cnt += 1
+        
+        return cnt
