1dcc0a99475f1c5bb270bf8143db320afc99e5de
qxzsilver1 <hk442@cornell.edu>
2026-08-21 10:50:04 -0400

    Create solution.py
    O(n) time and space solution, where n is the number of integers in both lists combined - using a hashmap.
    
    Create the number to position hashmap.
    
    Fill the hashmap with the respective position of the num2 array and the corresponding index.
    
    Create a mapping array initialized with 0's.
    
    For each index in nums1, set the mapping value to the value from the dictionary for that particular number.
    
    Return the mapping array.

---
+++ 760. Find Anagram Mappings/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num_to_pos = {}
+
+        for i in range(len(nums2)):
+            num_to_pos[nums2[i]] = i
+        
+        mapping = [0] * len(nums1)
+
+        for i in range(len(nums1)):
+            mapping[i] = num_to_pos[nums1[i]]
+        
+        return mapping
