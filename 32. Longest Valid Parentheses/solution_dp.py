2e19028291680d877ec86ca4d435ecd071e61538
qxzsilver1 <hk442@cornell.edu>
2026-08-24 17:30:09 -0400

    Create solution_dp.py
    O(n) time and space solution using dynamic programming and caching.
    
    This problem can be solved by using Dynamic Programming. We make use of a dp array where ith element of dp represents the length of the longest valid substring ending at ith index. We initialize the complete dp array with 0's. Now, it's obvious that the valid substrings must end with ‘)’. This further leads to the conclusion that the substrings ending with ‘(’ will always contain '0' at their corresponding dp indices. Thus, we update the dp array only when ‘)’ is encountered.
    
    To fill dp array we will check every two consecutive characters of the string and if
    
    s[i]=‘)’ and s[i−1]=‘(’, i.e. string looks like ‘.......()"⇒ dp[i]=dp[i−2]+2
    
    We do so because the ending "()" portion is a valid substring anyhow and leads to an increment of 2 in the length of the just previous valid substring's length.
    
    s[i]=‘)’ and s[i−1]=‘)’, i.e. string looks like ‘.......))"⇒ 
    if s[i−dp[i−1]−1]=‘(’ then dp[i]=dp[i−1]+dp[i−dp[i−1]−2]+2
    
    The reason behind this is that if the 2nd last ‘)’ was a part of a valid substring (say sub s), for the last ‘)’ to be a part of a larger substring, there must be a corresponding starting ‘(’ which lies before the valid substring of which the 2nd last ‘)’ is a part (i.e. before sub s). Thus, if the character before sub s happens to be ‘(’, we update the dp[i] as an addition of 2 in the length of sub s which is dp[i−1]. To this, we also add the length of the valid substring just before the term "(,sub_s,)" , i.e. dp[i−dp[i−1]−2].

---
+++ 32. Longest Valid Parentheses/solution_dp.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        max_val = 0
+
+        dp = [0] * len(s)
+        
+        for i in range(1, len(s)):
+            if s[i] == ')':
+                if s[i-1] == '(':
+                    dp[i] = (dp[i-2] if i >= 2 else 0) + 2
+                elif i - dp[i-1] > 0 and s[i - dp[i-1] - 1] == '(':
+                    dp[i] = dp[i-1] + (dp[i - dp[i-1] - 2] if i - dp[i-1] >= 2 else 0) + 2
+                
+                max_val = max(max_val, dp[i])
+        
+        return max_val
