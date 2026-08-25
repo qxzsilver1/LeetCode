40e06a88ac64832a9652be25fd2950afdbeb311b
qxzsilver1 <hk442@cornell.edu>
2026-08-25 01:13:02 -0400

    Create solution.py
    O(n) time and space solution using a hashset.
    
    Create an element set using the array.
    
    Initialize the count to 0.
    
    For each number a in the array, check if a+1 is also in the set, and increment the count if it is.
    
    Return the count.

---
+++ 1426. Counting Elements/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        element_set = set(arr)
+
+        count = 0
+
+        for a in arr:
+            if a + 1 in element_set:
+                count += 1
+        
+        return count
