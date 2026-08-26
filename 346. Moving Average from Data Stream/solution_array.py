f51778fe40769db6404d9a4f50020caf2ceb3917
qxzsilver1 <hk442@cornell.edu>
2026-08-26 19:47:45 -0400

    Create solution_array.py
    O(nm) time complexity and O(m) space complexity (for the queue), where m is the number of times the next() method is called, and n is the size of the window.

---
+++ 346. Moving Average from Data Stream/solution_array.py
@@ -0,0 +1,20 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.size = size
+        self.queue = []
+
+    def next(self, val: int) -> float:
+        size = self.size
+        queue = self.queue
+
+        queue.append(val)
+
+        window_sum = sum(queue[-size:])
+
+        return window_sum / min(len(queue), size)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
