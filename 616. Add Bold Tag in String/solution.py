b118d4c3ee3536ac8af5d064a3bda6ffc615348c
qxzsilver1 <hk442@cornell.edu>
2026-08-26 12:32:32 -0400

    Create solution.py
    O(m ( n^2 k - k^2 n)) time and O(n) space solution, where n is the length of the string, m is the length of the words array, and k is the average length of a word.
    
    The key insight is that we need to track which characters should be bold, not which substrings. If we find all occurrences of each word in the string and mark every character position that falls within any match, we can then merge overlapping or adjacent bold regions naturally.
    
    We use a boolean array where each index corresponds to a character in the string. For every word, we find all its occurrences and mark the corresponding positions as bold. When building the result, we only insert <b> at the start of a bold region and </b> at the end, which handles merging automatically.
    
    Create a boolean array bold of size n (length of string s), initialized to false.
    For each word in the dictionary:
    Find all occurrences of the word in s.
    For each occurrence starting at index start, mark bold[i] = true for all i from start to start + len(word) - 1.
    Build the result string by iterating through s:
    If bold[i] is true and either i == 0 or bold[i-1] is false, insert <b>.
    Append the character s[i].
    If bold[i] is true and either i == n-1 or bold[i+1] is false, insert </b>.
    Return the result string.

---
+++ 616. Add Bold Tag in String/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def addBoldTag(self, s: str, words: List[str]) -> str:
+        n = len(s)
+
+        bold = [False] * n
+
+        for word in words:
+            start = s.find(word)
+
+            while start != -1:
+                for i in range(start, start + len(word)):
+                    bold[i] = True
+                
+                start = s.find(word, start + 1)
+        
+        open_tag, close_tag = '<b>', '</b>'
+
+        res = []
+
+        for i in range(n):
+            if bold[i] and (i == 0 or not bold[i-1]):
+                res.append(open_tag)
+            
+            res.append(s[i])
+
+            if bold[i] and (i == n-1 or not bold[i+1]):
+                res.append(close_tag)
+        
+        return ''.join(res)
