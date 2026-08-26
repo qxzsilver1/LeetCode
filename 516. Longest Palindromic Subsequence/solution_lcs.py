9d3da9a0d2c2cee67254a6c5ed3fdf1047111fe6
qxzsilver1 <hk442@cornell.edu>
2026-08-26 18:41:21 -0400

    Create solution_lcs.py
    O(n^2) time and O(n^2) space solution using longest common subsequence dynamic programming idea.
    
    The idea is to perform the longest common subsequence (LCS) algorithm on the string s itself and the reversed string - this will always give a palindrome.

---
+++ 516. Longest Palindromic Subsequence/solution_lcs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        def longestCommonSubsequence(s1: str, s2: str) -> int:
+            N, M = len(s1), len(s2)
+            dp = [[0] * (M + 1) for _ in range(N + 1)]
+
+            for i in range(N):
+                for j in range(M):
+                    if s1[i] == s2[j]:
+                        dp[i + 1][j + 1] = 1 + dp[i][j]
+                    else:
+                        dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+
+            return dp[N][M]
+        
+        return longestCommonSubsequence(s, s[::-1])
