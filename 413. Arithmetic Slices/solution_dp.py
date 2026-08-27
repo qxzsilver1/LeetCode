5e5a851b65695654c30d7819f662a59b197d7d27
qxzsilver1 <hk442@cornell.edu>
2026-08-27 09:21:47 -0400

    Create solution_dp.py
    O(n) time and space solution using top-down dynamic programming.
    
    First, the DP array is initialized with 0, and the result is set to 0.
    
    For each number in index range from 2 to the end, if the triplet differences between i and (i-1), and (i-1) and (i-2) are the same, then the DP value at i is incremented by the DP value at (i-1) + 1. This value is then added to the result.
    
    Finally, the result is returned.

---
+++ 413. Arithmetic Slices/solution_dp.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+
+        res = 0
+
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:
+                dp[i] = 1 + dp[i - 1]
+                res += dp[i]
+        
+        return res
