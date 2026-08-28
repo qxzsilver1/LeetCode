b109e92cd7c0c308acbab300e988394f9f84b350
qxzsilver1 <hk442@cornell.edu>
2026-08-28 17:26:34 -0400

    Create solution_arr.py
    O(n) time and O(n) space (assuming output array counts for space) if not using the original input array and modifying it in-place.

---
+++ 1480. Running Sum of 1d Array/solution_arr.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+
+        res[0] = nums[0]
+        
+        for i in range(1, len(nums)):
+            res[i] += res[i - 1] + nums[i]
+        
+        return res
