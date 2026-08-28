ff06afc963b58a4a390885cabd860b286771686f
qxzsilver1 <hk442@cornell.edu>
2026-08-28 12:45:05 -0400

    Create solution.py
    O(nm^3) time and O(nm) space complexity solution using top-down dynamic programming with caching, where n is the size of the words array and m is the longest length of the word in the list.

---
+++ 472. Concatenated Words/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        word_set = set(words)
+        dp = {}
+
+        def dfs(word):
+            if word in dp:
+                return dp[word]
+
+            for i in range(1, len(word)):
+                prefix = word[:i]
+                suffix = word[i:]
+
+                if (prefix in word_set and suffix in word_set) or (prefix in word_set and dfs(suffix)):
+                    dp[word] = True
+                    return dp[word]
+            
+            dp[word] = False
+            return dp[word]
+        
+        res = []
+
+        for w in words:
+            if dfs(w):
+                res.append(w)
+        
+        return res
