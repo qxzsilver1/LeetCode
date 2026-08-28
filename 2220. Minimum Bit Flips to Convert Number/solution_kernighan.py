11d04114836f39632f0fb5f574d2692b2288502b
qxzsilver1 <hk442@cornell.edu>
2026-08-28 13:53:37 -0400

    Create solution_kernighan.py
    O(1) solution with slightly faster solution using subtracting 1 and bitwise AND the solution to get the next bit. This advances faster potentially for large gaps of 1's separated by 0's in bit representation.

---
+++ 2220. Minimum Bit Flips to Convert Number/solution_kernighan.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        res = 0
+
+        n = start ^ goal
+
+        while n:
+            n &= (n - 1)
+            res += 1
+        
+        return res
