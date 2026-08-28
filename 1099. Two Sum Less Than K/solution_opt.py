f5fee2d8522aef924461237208b83d50188453b8
qxzsilver1 <hk442@cornell.edu>
2026-08-28 15:58:14 -0400

    Create solution_opt.py
    O(n + m) time and O(m) space solution using counting (bucket) sort, with n being the number of integers in the array and m 1000 (for the range of possible input numbers from 1 to 1000).

---
+++ 1099. Two Sum Less Than K/solution_opt.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        res = -1
+        
+        count = [0] * 1001
+        
+        for num in nums:
+            count[num] += 1
+        
+        lo = 1
+        hi = 1000
+        
+        while lo <= hi:
+            if lo + hi >= k or count[hi] == 0:
+                hi -= 1
+            else:
+                if count[lo] > (0 if lo < hi else 1):
+                    res = max(res, lo + hi)
+                lo += 1
+        
+        return res
