16c6c3839f66de5c9628298ffbb1245deccd9ee1
qxzsilver1 <hk442@cornell.edu>
2026-09-04 19:07:32 -0400

    Create solution.py
    O(n) time and space solution.
    
    First, get the length of the number array.
    
    If the size of the array is 1, return 0.
    
    Create a max prefix array, a min suffix array, and stability score array all of size n (actual values inside don't matter).
    
    For each index from 0 to n - 1, fill in the max prefix array by finding the max of the value in the number array at index i and the previous max in the max prefix array at i - 1.
    
    Similarly, create the min suffix array from n - 1 to 0 by finding the min of the value in the number array at index i and the previous min in the min suffix array at index i + 1.
    
    Set the smallest stable index to -1.
    
    For each index, calculate the stability score.
    
    If the stability score is less than k, set it as the smallest stable index and return it (since the iteration is going for 0 to n -1, it will always be the smallest stable index if found.
    
    Return the smallest stable index.

---
+++ 3903. Smallest Stable Index I/solution.py
@@ -0,0 +1,34 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        if n == 1:
+            return 0
+        
+        max_prefix = [float('-inf')] * n
+        min_suffix = [float('inf')] * n
+        stability_score = [0] * n
+
+        for i in range(n):
+            if i == 0:
+                max_prefix[i] = nums[i]
+            else:
+                max_prefix[i] = max(nums[i], max_prefix[i-1])
+        
+        for i in range(n - 1, -1, -1):
+            if i == n - 1:
+                min_suffix[i] = nums[i]
+            else:
+                min_suffix[i] = min(nums[i], min_suffix[i+1])
+        
+        smallest_stable_idx = -1
+
+        for i in range(n):
+            stability_score[i] = max_prefix[i] - min_suffix[i]
+
+            if stability_score[i] <= k:
+                smallest_stable_idx = i
+                return smallest_stable_idx
+        
+        return smallest_stable_idx
+        
