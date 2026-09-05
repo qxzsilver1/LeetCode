8719221772ff8d5b1ac8bd12dceb307fadf87b1f
qxzsilver1 <hk442@cornell.edu>
2026-09-04 23:12:50 -0400

    Create solution_trie.py
    O(n^2 + mk) time and O(n + mk) space solution using top-down dynamic programming with a trie for the dictionary word matching.
    
    Instead of the hashset, a trie is used for traversal and matching, which speeds up the lookup.
    
    First, create the DP cache initialized with the base case - key value of length of string s is 0.
    
    Create a trie with the words from the dictionary, and set the root as a variable.
    
    Define the DFS function starting at index i. If i is already in the DP, then return the value.
    
    Set the result to 1 + the DFS run at the next index i + 1. 
    
    Set the current node to the trie (root) node.
    
    For j from i to the end of the string, if the character at index j is not in the current trie node's children, break. 
    
    Set the current node to the child node of character in the string s at index j.
    
    If the current node is the end of a word, then update the result to the minimum of itself and the DFS run at j + 1.
    
    Set the DP cache at i to the result and return it.
    
    Return the DFS run at index 0.

---
+++ 2707. Extra Characters in a String/solution_trie.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+    def __init__(self, words):
+        self.root = TrieNode()
+
+        for w in words:
+            curr = self.root
+
+            for c in w:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.is_word = True
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        dp = { len(s) : 0}
+
+        trie = Trie(dictionary).root
+
+        def dfs(i):         
+            if i in dp:
+                return dp[i]
+            
+            res = 1 + dfs(i + 1)
+            curr = trie
+
+            for j in range(i, len(s)):
+                if s[j] not in curr.children:
+                    break
+                curr = curr.children[s[j]]
+
+                if curr.is_word:
+                    res = min(res, dfs(j + 1))
+            
+            dp[i] = res
+            return res
+        
+        return dfs(0)
