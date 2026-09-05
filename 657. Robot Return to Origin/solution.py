2400ed3c8867c3691a97d4a62ec99de3d248fee8
qxzsilver1 <hk442@cornell.edu>
2026-09-05 13:57:01 -0400

    Create solution.py
    O(n) time and O(1) space solution.
    
    Set the x and y coordinates to 0.
    
    For each move, increment/decrement the x and y coordinates appropriately depending on the move.
    
    Return whether both x and y are 0.

---
+++ 657. Robot Return to Origin/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x, y = 0, 0
+
+        for move in moves:
+            if move == 'U':
+                y += 1
+            
+            if move == 'D':
+                y -= 1
+            
+            if move == 'L':
+                x -= 1
+            
+            if move == 'R':
+                x += 1
+        
+        return x == 0 and y == 0
