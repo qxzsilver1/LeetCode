ed664b9bd0eaa1e6c7525a97179e761d95d434ab
qxzsilver1 <hk442@cornell.edu>
2026-09-05 16:21:00 -0400

    Create solution.py
    O(n) time and space solution using division and modulus operator with respect to 7.

---
+++ 504. Base 7/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        n = abs(num)
+        res = ''
+
+        while n:
+            res = str(n % 7) + res
+            n //= 7
+        
+        return '-' * (num < 0) + res or '0'
