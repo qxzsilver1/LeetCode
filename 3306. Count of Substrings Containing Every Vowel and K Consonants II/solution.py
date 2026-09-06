bd9f925aa118bdaf4a5bd8a78cfcaa75723cdade
qxzsilver1 <hk442@cornell.edu>
2026-09-06 01:40:34 -0400

    Create solution.py
    O(n) time and O(1) space solution using the same logic as leetcode 3305 - count of substrings containing every vowel and k consonants I (this is actually the same exact problem).

---
+++ 3306. Count of Substrings Containing Every Vowel and K Consonants II/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        def atLeastK(k):
+            vowels = defaultdict(int)
+            non_vowel_cnt = 0
+
+            res = 0
+
+            l = 0
+
+            for r in range(len(word)):
+                if word[r] in 'aeiou':
+                    vowels[word[r]] += 1
+                else:
+                    non_vowel_cnt += 1
+                
+                while len(vowels) == 5 and non_vowel_cnt >= k:
+                    res += (len(word) - r)
+
+                    if word[l] in 'aeiou':
+                        vowels[word[l]] -= 1
+                    else:
+                        non_vowel_cnt -= 1
+                    
+                    if vowels[word[l]] == 0:
+                        vowels.pop(word[l])
+                    
+                    l += 1
+
+            return res
+        
+        return atLeastK(k) - atLeastK(k + 1)
