5b861658636c34e09cac32e5da6198e03c4da207
qxzsilver1 <hk442@cornell.edu>
2026-09-06 14:35:22 -0400

    Create solution_wrong.py
    O(n) time and O(1) space solution - this is wrong, since the while loop fails when the element is divisible by 10 (so the loop won't execute). Example failure case is when the number is 100 or 10, etc.

---
+++ 1085. Sum of Digits in the Minimum Number/solution_wrong.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumOfDigits(self, nums: List[int]) -> int:
+        min_elem = min(nums)
+
+        digit_sum = 0
+
+        while min_elem % 10:
+            digit_sum += min_elem % 10
+            min_elem //= 10
+        
+        return 1 if digit_sum % 2 == 0 else 0
