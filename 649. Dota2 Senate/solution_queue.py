b6daae2f05ae1a630756485609d0e2ebb58ba8d5
qxzsilver1 <hk442@cornell.edu>
2026-09-06 19:53:01 -0400

    Create solution_queue.py
    O(n) time and space solution using a single queue instead of two queues.
    
    Counters and floating bans for each party is counted in the iteration to keep track of bans.

---
+++ 649. Dota2 Senate/solution_queue.py
@@ -0,0 +1,28 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        r_count = senate.count('R')
+        d_count = len(senate) - r_count
+
+        d_floating_ban, r_floating_ban = 0, 0
+
+        q = deque(senate)
+
+        while r_count and d_count:
+            curr = q.popleft()
+
+            if curr == 'D':
+                if d_floating_ban:
+                    d_floating_ban -= 1
+                    d_count -= 1
+                else:
+                    r_floating_ban += 1
+                    q.append('D')
+            else:
+                if r_floating_ban:
+                    r_floating_ban -= 1
+                    r_count -= 1
+                else:
+                    d_floating_ban += 1
+                    q.append('R')
+        
+        return 'Radiant' if r_count else 'Dire'
