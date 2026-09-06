426d4938435d91ca641fe6432b04da222d442252
qxzsilver1 <hk442@cornell.edu>
2026-09-06 15:12:36 -0400

    Create solution_dp_opt.py
    O(n^2) time and O(n) space solution using bottom-up dynamic programming with a single array for each iteration.

---
+++ 877. Stone Game/solution_dp_opt.py
@@ -0,0 +1,20 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        n = len(piles)
+        dp = [0] * n
+
+        for l in reversed(range(n)):
+            for r in range(l, n):
+                even = ((r - l) % 2 == 0)
+                left = piles[l] if even else 0
+                right = piles[r] if even else 0
+
+                if l == r:
+                    dp[r] = left
+                else:
+                    dp[r] = max(dp[r] + left, dp[r - 1] + right)
+
+        total = sum(piles)
+        alice_score = dp[n - 1]
+        
+        return alice_score > (total - alice_score)
