cb17d1e1a4748a718f27c0f5563499301a0e9df0
qxzsilver1 <hk442@cornell.edu>
2026-09-06 13:24:10 -0400

    Create solution_greedy.py
    O(n) time and O(1) space solution using a greedy approach with XOR to check parity.
    
    To find if the last character in the array is a one-bit character, we can use a parity-based approach. First, we remove the last element, which is the character we want to check. We then initialize a parity variable and iterate backward through the array. Each time we encounter a 1, we toggle parity with parity ^= 1, effectively flipping its value. This toggle allows us to track whether the number of 1s is odd or even.
    
    At the end, if parity is 0, it indicates the last character is a one-bit character; if parity is 1, it’s part of a two-bit sequence.

---
+++ 717. 1-bit and 2-bit Characters/solution_greedy.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        parity = bits.pop()
+
+        while bits and bits.pop():
+            parity ^= 1
+        
+        return parity == 0
