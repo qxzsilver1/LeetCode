c6124a6ec7356d54b61ca030f09c53168ea6813c
qxzsilver1 <hk442@cornell.edu>
2026-09-07 00:27:31 -0400

    Create solution.sql

---
+++ 1142. User Activity for the Past 30 Days II/MySQL/solution.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT IFNULL(ROUND(COUNT(DISTINCT session_id)/COUNT(DISTINCT user_id), 2), 0.00) AS average_sessions_per_user
+FROM Activity
+WHERE DATEDIFF('2019-07-27', activity_date) < 30 AND DATEDIFF('2019-07-27', activity_date) >= 0;
