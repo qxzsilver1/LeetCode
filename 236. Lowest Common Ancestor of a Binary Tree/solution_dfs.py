8917c14124f928db983822955c0a0193e94b61f7
qxzsilver1 <hk442@cornell.edu>
2026-09-07 13:59:32 -0400

    Create solution_dfs.py
    O(n) time and space solution using iterative BFS (queue).

---
+++ 236. Lowest Common Ancestor of a Binary Tree/solution_dfs.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        parent = {root: None}
+        queue = deque([root])
+        
+        while p not in parent or q not in parent:
+            node = queue.popleft()
+            
+            if node.left:
+                parent[node.left] = node
+                queue.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                queue.append(node.right)
+
+        ancestors = set()
+        
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+
+        while q not in ancestors:
+            q = parent[q]
+
+        return q
