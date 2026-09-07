3e08bca8417d2807a8bd02db6113573919f17e5d
qxzsilver1 <hk442@cornell.edu>
2026-09-07 13:56:42 -0400

    Create solution_bfs.py
    O(n) time and O(log n) space solution using iterative BFS with a queue, where log n is the width of the tree.

---
+++ 116. Populating Next Right Pointers in Each Node/solution_bfs.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+
+        q = deque([root])
+        
+        while q:
+            levelSize = len(q)
+            
+            while levelSize:
+                node = q.popleft()
+                if levelSize > 1:
+                    node.next = q[0]
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                levelSize -= 1
+
+        return root
