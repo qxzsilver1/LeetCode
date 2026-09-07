12e6ccb345500c3aef7ae28037898295917c3e9c
qxzsilver1 <hk442@cornell.edu>
2026-09-06 23:55:53 -0400

    Create solution_window.sql
    Multiple window functions can be used to get the top/max value from a column: FIRST_VALUE, RANK, ROW_NUMBER, etc., but the underlying logic is the same: We sort the columns in a specific order, for instance in this question, we are interested in the maximum experience_years. Thus, we sort that table in reverse order based on experience_years, so that the desired maximum value will be placed at rank 1.
    
    In the subquery, JOIN the two tables and give each record a rank by the experience_years in descending order for each project.
    In the main query, SELECT only the project and employee from the subquery with the rank equals 1.

---
+++ 1077. Project Employees III/MySQL/solution_window.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT project_id, employee_id
+FROM (
+    SELECT p.project_id, p.employee_id, RANK() OVER (PARTITION BY project_id ORDER BY experience_years DESC) AS rnk
+    FROM Project p JOIN Employee e ON p.employee_id = e.employee_id
+) a
+WHERE rnk = 1;
