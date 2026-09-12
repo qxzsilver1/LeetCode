629132204f1e93db8c212af376c80a042117c9f2
qxzsilver1 <hk442@cornell.edu>
2026-09-12 14:19:24 -0400

    Create solution_opt.py
    O(n (k + log n)) time and O(nk) space solution using bottom-up dynamic programming with a 2D array and binary search.
    
    The difference here is that compared to the other bottom-up dynamic programming approach, the next index calculation is done once in the outer for loop (as opposed to each time in the inner for loop), saving time complexity.

---
+++ 1751. Maximum Number of Events That Can Be Attended II/solution_opt.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        events.sort()
+
+        n = len(events)
+
+        start_times = [start for start, _, _ in events]
+
+        dp = [[0] * (n + 1) for _ in range(k + 1)]
+
+        for i in range(n - 1, -1, -1):
+            next_idx = bisect_right(start_times, events[i][1])
+
+            for cnt in range(1, k + 1):
+                dp[cnt][i] = max(dp[cnt][i + 1], events[i][2] + dp[cnt - 1][next_idx])
+        
+        return dp[k][0]
