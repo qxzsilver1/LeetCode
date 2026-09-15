0caea47c44988f67c072a73b65f66c1f60dbd000
qxzsilver1 <hk442@cornell.edu>
2026-09-15 13:22:55 -0400

    Create solution.py
    O(n log n) time and O(n) space solution using the zipped list of intervals and sorting.
    
    The bisection does binary search for the left and right points for the length of intersections.

---
+++ 3893. Maximum Team Size with Overlapping Intervals/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumTeamSize(self, startTime: list[int], endTime: list[int]) -> int:
+        intervals = list(zip(startTime, endTime))
+
+        res = 0
+
+        startTime.sort()
+        endTime.sort()
+
+        for beg, end in intervals:
+            l = bisect_left(endTime, beg)
+            r = bisect_right(startTime, end)
+
+            res = max(res, r - l)
+        
+        return res
