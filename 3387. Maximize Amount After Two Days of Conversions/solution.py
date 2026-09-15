b90f5e22004cd46f5ec0e70ca8bf21548b8d0f6b
qxzsilver1 <hk442@cornell.edu>
2026-09-15 13:09:06 -0400

    Create solution.py
    O(E + V) time and O(V) space solution using Bellman-Ford to go through each pair for the first day, then the second day, and get the best conversion.
    
    This gives the wrong solution and fails - it needs to be a double-loop to go through n^2 pairs each time in the Bellman-Ford algorithm.

---
+++ 3387. Maximize Amount After Two Days of Conversions/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxAmount(self, initialCurrency: str, pairs1: List[List[str]], rates1: List[float], pairs2: List[List[str]], rates2: List[float]) -> float:
+        bestConversion = defaultdict(int)
+        bestConversion[initialCurrency] = 1
+
+        def bellmanFord(pairs, rates):
+            n = len(pairs)
+            for i in range(n):
+                bestConversion[pairs[i][1]] = max(bestConversion[pairs[i][1]], bestConversion[pairs[i][0]] * rates[i])
+                bestConversion[pairs[i][0]] = max(bestConversion[pairs[i][0]], bestConversion[pairs[i][1]] / rates[i])
+        
+        bellmanFord(pairs1, rates1)
+        bellmanFord(pairs2, rates2)
+
+        return bestConversion[initialCurrency]
