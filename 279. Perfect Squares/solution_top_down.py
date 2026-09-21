20c5a59190bd8e3e18f3759e26cbd51e0ee826c6
qxzsilver1 <hk442@cornell.edu>
2026-09-21 19:59:52 -0400

    Create solution_top_down.py
    O(n sqrt(n)) time and O(n) space solution using top-down dynamic programming (memoization).

---
+++ 279. Perfect Squares/solution_top_down.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        memo = {}
+
+        def dfs(target):
+            if target == 0:
+                return 0
+            if target in memo:
+                return memo[target]
+
+            res = target
+            for i in range(1, target + 1):
+                if i * i > target:
+                    break
+                res = min(res, 1 + dfs(target - i * i))
+
+            memo[target] = res
+            return res
+
+        return dfs(n)
