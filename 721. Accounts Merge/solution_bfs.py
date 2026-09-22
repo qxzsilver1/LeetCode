db7afda69803c763dd004632615298c77a278604
qxzsilver1 <hk442@cornell.edu>
2026-09-22 00:10:52 -0400

    Create solution_bfs.py
    O(mn log(mn)) time and O(mn) space solution using BFS, where m is the number of emails, and n is the number of accounts.

---
+++ 721. Accounts Merge/solution_bfs.py
@@ -0,0 +1,53 @@
+class Solution:
+    def accountsMerge(self, accounts: list[list[str]]) -> list[list[str]]:
+        n = len(accounts)
+        email_idx = {} # email -> id
+        emails = [] # set of emails of all accounts
+        email_to_acc = {} # email_index -> account_Id
+
+        m = 0
+        
+        for acc_idx, a in enumerate(accounts):
+            for i in range(1, len(a)):
+                email = a[i]
+                if email in email_idx:
+                    continue
+                emails.append(email)
+                email_idx[email] = m
+                email_to_acc[m] = acc_idx
+                m += 1
+
+        adj = [[] for _ in range(m)]
+        
+        for a in accounts:
+            for i in range(2, len(a)):
+                id1 = email_idx[a[i]]
+                id2 = email_idx[a[i - 1]]
+                adj[id1].append(id2)
+                adj[id2].append(id1)
+
+        email_groups = defaultdict(list) # index of acc -> list of emails
+        visited = [False] * m
+
+        def bfs(start, acc_idx):
+            queue = deque([start])
+            visited[start] = True
+            while queue:
+                node = queue.popleft()
+                email_groups[acc_idx].append(emails[node])
+                for nei in adj[node]:
+                    if not visited[nei]:
+                        visited[nei] = True
+                        queue.append(nei)
+
+        for i in range(m):
+            if not visited[i]:
+                bfs(i, email_to_acc[i])
+
+        res = []
+        
+        for acc_idx in email_groups:
+            name = accounts[acc_idx][0]
+            res.append([name] + sorted(email_groups[acc_idx]))
+
+        return res
