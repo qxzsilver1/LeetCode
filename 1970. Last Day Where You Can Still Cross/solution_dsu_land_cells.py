377c0bb968edddd4a6a0cf09a29ed87ba75845f6
qxzsilver1 <hk442@cornell.edu>
2026-09-23 10:15:03 -0400

    Create solution_dsu_land_cells.py
    O(rc) time and space solution using disjoint set union on land cells.
    
    We need to reverse the days cells, which equals replacing water cells with land cells. This is necessary because we are searching for the last day when there is a path, which is the same as the first day in reversed order. During the union-find process, will we traverse through cells in reverse and replace the corresponding water cell cells[i] by land cell. For each newly added land cell, we connect it with all of its neighboring land cells. We repeat this process until either the first row and the last row are connected or the traversal is complete.
    
    During the iteration, for each water cell cells[i] that will be replaced by land, in addition to connecting it with its land neighbors, we will also:
    
    connect it with top if it is in the first row.
    connect it with bottom if it is in the last row.
    Therefore, we can simply check if the first row is connected with the bottom row after this day, by checking if top and bottom are connected.
    
    Algorithm
    Create a disjoint set data structure dsu with a size of row * col + 2.
    
    Create an all-one grid of size row * col representing the water cells after the last day.
    
    Iterate over reversed cells, for each cell cells[i] = (r, c):
    
    Check its neighbors in all four directions, and if there is a land cell (new_r, new_c), we connect the root of cells[i] to the root of this neighbor in dsu.
    
    If r = 0, connect it with top.
    
    If r = row - 1, connect it with bottom.
    
    Check if top and bottom are connected, and return i if they are.

---
+++ 1970. Last Day Where You Can Still Cross/solution_dsu_land_cells.py
@@ -0,0 +1,49 @@
+class DSU:
+    def __init__(self, n):
+        self.root = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return
+        
+        if self.size[root_x] > self.size[root_y]:
+            root_x, root_y = root_y, root_x
+        self.root[root_x] = root_y
+        self.size[root_y] += self.size[root_x]
+
+class Solution:
+    def latestDayToCross(self, row: int, col: int, cells: list[list[int]]) -> int:
+        grid = [[1] * col for _ in range(row)]
+
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        dsu = DSU(row * col + 2)
+
+        for i in range(len(cells) - 1, -1, -1):
+            r, c = cells[i][0] - 1, cells[i][1] - 1
+            grid[r][c] = 0
+            idx1 = r * col + c + 1
+
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                idx2 = nr * col + nc + 1
+
+                if 0 <= nr < row and 0 <= nc < col and grid[nr][nc] == 0:
+                    dsu.union(idx1, idx2)
+            
+            if r == 0:
+                dsu.union(0, idx1)
+            
+            if r == row - 1:
+                dsu.union(row * col + 1, idx1)
+            
+            if dsu.find(0) == dsu.find(row * col + 1):
+                return i
