1a9172350272913d9f2a35f64221165aeb0c06b4
qxzsilver1 <hk442@cornell.edu>
2026-09-24 08:24:36 -0400

    Create solution_combinatorics.py
    O(n^2) time and space solution using combinatorics.
    
    Each element in Pascal's Triangle corresponds to a binomial coefficient. The value at row n and position k is C(n, k). Rather than computing each coefficient from scratch using factorials, we can compute them incrementally. Starting from 1, each subsequent value in a row can be derived by multiplying by (n - k + 1) / k.
    
    First, initialize the result array.
    
    For each number up to the number of rows, initialize a row array with a 1. Set the value to 1.
    
    For each k from 1 to n, multiply the value by (n - k + 1)/k integer-wise, then append to the row. This successively multiples the factor to the binomial coefficients in the next iteration.
    
    Append this row to the result.
    
    Return the result.

---
+++ 118. Pascal's Triangle/solution_combinatorics.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        res = []
+        
+        for n in range(numRows):
+            row = [1]
+            val = 1
+            
+            for k in range(1, n + 1):
+                val = val * (n - k + 1) // k
+                row.append(val)
+            res.append(row)
+        return res
